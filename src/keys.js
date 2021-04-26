5665ffcabbccddb5b994d55a5a5a6993c458753d
Patricio Cadenas <patova77@gmail.com>
2021-04-25 22:19:55 -0300

    fix

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
     mongodb: {
-        URI: 'mongodb://localhost:27017/example1'
+        URI: 'mongodb+srv://pato:1234@cluster0.lsy6r.mongodb.net/example?retryWrites=true&w=majority'
     }
 }
+
+//mongodb://localhost:27017/example1
